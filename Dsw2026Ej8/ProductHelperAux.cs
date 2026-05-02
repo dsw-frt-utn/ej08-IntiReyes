bde79e97abf0766b1bd95129ff4e3bd6e335012a
Inti Reyes <Inti.ReyesHerrera@alu.frt.edu.ar>
2026-05-02 12:57:01 -0300

    feat: problema 1 - clase parcial ProductHelper

---
+++ Dsw2026Ej8/ProductHelperAux.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8;
+
+public partial class ProductHelper
+{
+    private string FormatearPrecio(decimal price)
+    {
+        return price.ToString("C");
+    }
+}
