a90331124bb69e580f88af5ac3b39fd2e502dc19
Inti Reyes <Inti.ReyesHerrera@alu.frt.edu.ar>
2026-05-02 14:10:44 -0300

    feat: problema 4 - calcular promedio con notas nullable

---
+++ Dsw2026Ej8/Problema4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8;
+
+public class Problema4
+{
+    public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+    {
+        var notas = new[] { nota1, nota2, nota3 };
+
+        double suma = 0;
+        int cantidad = 0;
+
+        foreach (var nota in notas)
+        {
+            if (nota.HasValue && nota.Value >= 0 && nota.Value <= 10)
+            {
+                suma += nota.Value;
+                cantidad++;
+            }
+        }
+
+        if (cantidad == 0) return 0;
+
+        return suma / cantidad;
+    }
+}
