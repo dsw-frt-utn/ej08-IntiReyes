1055556eea54a8705a8f4cda6fdc3cb762e7c5f3
Inti Reyes <Inti.ReyesHerrera@alu.frt.edu.ar>
2026-05-02 14:06:28 -0300

    feat: problema 3 - comparar copias de valor y referencia

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8;
+
+public class Problema3
+{
+    public string CompararCopias(int originalValue, Product product)
+    {
+        // Copia del valor (tipo por valor)
+        int copia = originalValue;
+        copia++;
+
+        // Copia de la referencia (tipo por referencia)
+        Product productoCopia = product;
+        productoCopia.SetDescription("Descripcion modificada");
+
+        return $"{originalValue}-{copia}-{product.Description}";
+    }
+}
+
